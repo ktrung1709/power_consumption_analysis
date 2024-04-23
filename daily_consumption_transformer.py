3ab52c3da305c011297bb5272c2c80d4ebee72a6
ktrung1709 <ktrung1709@gmail.com>
2024-04-23 09:21:11 +0700

    Fix transformer to infer correct schema

--- daily_consumption_transformer.py
+++ daily_consumption_transformer.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, FloatType
+from pyspark.sql.functions import col
 
 conf = SparkConf()
 conf.set('spark.hadoop.fs.s3a.access.key','AKIATMFNNGPO53WMF6WR')
@@ -13,12 +15,24 @@ spark = SparkSession.builder.config(conf=conf).getOrCreate()
 # Define the S3 bucket path containing the JSON files
 s3_bucket_path = "s3a://electricity-consumption-master-data/power_consumption_data_20230101*.json"
 
-# Read JSON files into DataFrame
-df = spark.read.json(s3_bucket_path)
+schema = StructType([
+    StructField("meter_id", StringType(), nullable=False),
+    StructField("measure", FloatType(), nullable=False),
+    StructField("datetime_measured", TimestampType(), nullable=False)
+])
 
+# Read JSON files into DataFrame
+df = spark.read.schema(schema).json(s3_bucket_path)
+df.printSchema()
+df.show()
+
+df = df.withColumn("meter_id", col("meter_id").cast("int"))
+
+df.printSchema()
+df.show()
 # Calculate total consumption by group and show all data
 total_consumption = df.groupBy("meter_id").sum("measure").orderBy("meter_id")
-total_consumption.show(df.count(), False)
+total_consumption.show()
 
 # Stop SparkSession
 spark.stop()
